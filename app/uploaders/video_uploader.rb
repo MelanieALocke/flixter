86b6c0eed983f287b2500b3143669525c26b2846
Melanie Locke <melanie.a.locke@gmail.com>
2019-10-02 02:35:55 +0000

    Video uploader created, and field for video added to instructor lesson page and student lesson show page

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,10 +1,11 @@
-class PhotoUploader < CarrierWave::Uploader::Base
+class VideoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
+  # storage :fog
   storage :aws
 
   # Override the directory where uploaded files will be stored.
@@ -22,7 +23,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [800, 350]
+  # process scale: [200, 300]
   #
   # def scale(width, height)
   #   # do something
