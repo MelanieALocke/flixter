0c0ebfd6b1f7f8def4adcc8032113ddc715b3663
Melanie Locke <melanie.a.locke@gmail.com>
2019-10-05 21:14:00 +0000

    Add Gem ranked-model and new column to db to order lessons

---
+++ db/migrate/20191005204948_alter_lesson_add_row_order.rb
@@ -0,0 +1,8 @@
+class AlterLessonAddRowOrder < ActiveRecord::Migration[5.2]
+  
+  def change
+    add_column :lessons, :row_order, :integer
+    add_index :lessons, :row_order
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_012540) do
+ActiveRecord::Schema.define(version: 2019_10_05_204948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2019_10_04_012540) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "video"
+    t.integer "row_order"
+    t.index ["row_order"], name: "index_lessons_on_row_order"
     t.index ["section_id"], name: "index_lessons_on_section_id"
   end
 
